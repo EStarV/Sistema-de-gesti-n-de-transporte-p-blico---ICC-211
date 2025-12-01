0afbbcc8bd09887d4bdfac2c0737122e1294e086
Estarlin Valerio <valerioestarlin4tog@gmail.com>
2025-11-30 23:20:43 -0400

    Cambio en el grafo para soportar implementaciones

--- src/main/java/com/example/proyectofinalicc211/logico/ListaAdyacencia.java
+++ src/main/java/com/example/proyectofinalicc211/logico/ListaAdyacencia.java
@@ -6,13 +6,31 @@ public class ListaAdyacencia {
     private Map<UUID, List<Ruta>> grafo = new HashMap<>();
     private Map<UUID, Parada> paradas = new HashMap<>();
 
+    //Mapas para poder hacer comprobaciones
+    private Map<String, UUID> nombres = new HashMap<>();
+    private Map<UUID, List<Ruta>> entrantes = new HashMap<>();
+
+    public Map<UUID, List<Ruta>> getEntrantes(){
+        return entrantes;
+    }
+
     public ListaAdyacencia() {}
+
+    private static ListaAdyacencia listaAdyacencia = null;
+    public static ListaAdyacencia getInstancia() {
+        if (listaAdyacencia == null) {
+            listaAdyacencia = new ListaAdyacencia();
+        }
+        return listaAdyacencia;
+    }
 
     public void addParada(Parada parada){
         if(parada == null || parada.getId() == null) return;
         paradas.put(parada.getId(), parada);
         grafo.put(parada.getId(), new LinkedList<>());
-        grafo.put(parada.getId(), grafo.get(parada.getId()));
+        //Esto puede ser temporal para obtener comprobaciones en O(1)
+        nombres.put(parada.getNombre().toLowerCase(), parada.getId());
+        entrantes.put(parada.getId(), new LinkedList<>());
     }
 
     public boolean addRuta(Ruta ruta){
@@ -20,6 +38,7 @@ public class ListaAdyacencia {
         if(existeRutaEntreNodos(ruta.getId_origen(), ruta.getId_destino(), ruta.getMedio())) return false;
         if(grafo.containsKey(ruta.getId_origen())){
             grafo.get(ruta.getId_origen()).add(ruta);
+            entrantes.get(ruta.getId_destino()).add(ruta);
             return true;
         }
         return false;
@@ -27,16 +46,32 @@ public class ListaAdyacencia {
 
     public boolean deleteParada(Parada parada){
         if(parada == null || parada.getId() == null) return false;
-        return deleteParadaById(parada.getId());
+        return deleteParadaById(parada.getId(), parada.getNombre().toLowerCase());
     }
 
-    public boolean deleteParadaById(UUID id){
-        if(paradas.containsKey(id)){
-            grafo.remove(id);
-            for(List<Ruta> rutas : grafo.values()){
-                rutas.removeIf(r -> r.getId_destino().equals(id));
+    public boolean deleteParadaById(UUID idParada, String nombre){
+        if(idParada == null) return false;
+        if(paradas.containsKey(idParada)){
+            List<Ruta> salen = grafo.get(idParada);
+            List<Ruta> entran = entrantes.get(idParada);
+            //Eliminar en grafo salientes con destino al eliminado
+            if(entran != null && !entran.isEmpty()){
+                for(Ruta ruta : entran){
+                    grafo.get(ruta.getId_origen()).remove(ruta);
+                }
             }
-            paradas.remove(id);
+            //Elimina las rutas que llegan al nodo eliminado
+            if(!(salen == null || salen.isEmpty())){
+                for(Ruta ruta : salen){
+                    entrantes.get(ruta.getId_destino()).remove(ruta);
+                }
+            }
+            grafo.remove(idParada);
+            entrantes.remove(idParada);
+            paradas.remove(idParada);
+            if(nombre != null && !nombre.isEmpty()){
+                nombres.remove(nombre.toLowerCase());
+            }
             return true;
         }
         return false;
@@ -46,9 +81,20 @@ public class ListaAdyacencia {
         if(ruta == null || ruta.getId() == null || ruta.getId_origen() == null || ruta.getId_destino() == null) return false;
 
         UUID id_Origen = ruta.getId_origen();
+        UUID id_Destino = ruta.getId_destino();
+
         List<Ruta> rutas = grafo.get(id_Origen);
-        if(rutas == null) return false;
-        return rutas.removeIf(r -> r.getId().equals(ruta.getId()));
+        List<Ruta> entran = entrantes.get(id_Destino);
+
+        boolean eliminarSal = false;
+        boolean eliminarEnt = false;
+        if(rutas != null && !rutas.isEmpty()){
+            eliminarSal = rutas.removeIf(r -> r.getId().equals(ruta.getId()));
+        }
+        if(entran != null && !entran.isEmpty()){
+            eliminarEnt = entran.removeIf(r -> r.getId().equals(ruta.getId()));
+        }
+        return eliminarSal || eliminarEnt;
     }
 
     public Map<UUID, Parada> getParadas(){
@@ -87,6 +133,21 @@ public class ListaAdyacencia {
         return null;
     }
 
+    public Ruta buscarRutaConMedio(UUID origen, UUID destino, String medio){
+        List<Ruta> rutas = grafo.get(origen);
+        Ruta ruta = null;
+        if(rutas != null && !rutas.isEmpty()){
+            for(Ruta r: rutas){
+                if(r.getId_destino().equals(destino) && r.getMedio().equalsIgnoreCase(medio)){
+                    ruta = r;
+                    break;
+                }
+            }
+        }
+        return ruta;
+
+    }
+
     public Camino dijkstra(Parada origen, Parada destino, String parametro){
         Dijkstra dijkstra = new Dijkstra(grafo, paradas);
         return dijkstra.calcCamino(origen, destino, parametro);
@@ -115,10 +176,20 @@ public class ListaAdyacencia {
     public boolean modificarParada(Parada parada, String new_nombre){
         if(parada == null || parada.getId() == null) return false;
         if(!paradas.containsKey(parada.getId())) return false;
+        if(new_nombre == null || new_nombre.isEmpty()) return false;
+        String nombreViejo = parada.getNombre();
         paradas.get(parada.getId()).setNombre(new_nombre);
+        nombres.remove(nombreViejo);
+        nombres.put(new_nombre, parada.getId());
         return true;
     }
 
+    public boolean existeNombreParada(String nombre){
+        if(nombres.containsKey(nombre.toLowerCase())) return true;
+        return false;
+    }
+
+    //Modificar los atributos de la ruta
     public boolean modificarRuta(Ruta ruta, int new_tiempo, double new_distancia, int new_costo, int new_transbordos, String new_medio){
         if(ruta == null || ruta.getId() == null) return false;
 
@@ -135,6 +206,7 @@ public class ListaAdyacencia {
         return true;
     }
 
+    //Modificar los nodos de una ruta
     public boolean modifNodosRuta(Parada new_origen, Parada new_destino, Ruta ruta){
         if(new_origen == null || new_destino == null) return false;
         if(!paradas.containsKey(new_origen.getId()) || !paradas.containsKey(new_destino.getId())) return false;
@@ -142,10 +214,23 @@ public class ListaAdyacencia {
 
         Ruta r = getRuta(ruta.getId_origen(), ruta.getId());
         if(r == null) return false;
-        grafo.get(ruta.getId_origen()).remove(r);
+        List<Ruta> salViejo = grafo.get(r.getId_origen());
+        if (salViejo != null) {
+            salViejo.removeIf(x -> x.getId().equals(r.getId()));
+        }
+        List<Ruta> entViejo = entrantes.get(r.getId_destino());
+        if (entViejo != null) {
+            entViejo.removeIf(x -> x.getId().equals(r.getId()));
+        }
+
+        grafo.get(r.getId_origen()).remove(r);
+        entrantes.get(r.getId_destino()).remove(r);
+
         r.setId_origen(new_origen.getId());
         r.setId_destino(new_destino.getId());
+
         grafo.get(new_origen.getId()).add(ruta);
+        entrantes.get(new_destino.getId()).add(ruta);
         return true;
     }
 
@@ -163,6 +248,7 @@ public class ListaAdyacencia {
 
     public boolean existeRutaEntreNodos(UUID origen, UUID destino, String medio){
         List<Ruta>rutas = grafo.get(origen);
+        if(rutas == null) return false;
         for(Ruta r : rutas){
             if(r.getId_destino().equals(destino) && r.getMedio().equals(medio)){
                 return true;
@@ -171,4 +257,15 @@ public class ListaAdyacencia {
         return false;
     }
 
+    public List<Ruta> rutasEntreNodos(UUID origen, UUID destino){
+        List<Ruta>rutas = grafo.get(origen);
+        List<Ruta>rutasAux = new ArrayList<>();
+        if(rutas == null) return rutasAux;
+        for(Ruta r : rutas){
+            if(r.getId_destino().equals(destino)){
+                rutasAux.add(r);
+            }
+        }
+        return rutasAux;
+    }
 }
